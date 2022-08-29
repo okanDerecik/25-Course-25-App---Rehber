513d2b297e9b762b75fdcaf2ea2a92e31e5ce4da
okanDerecik <96899474+okanDerecik@users.noreply.github.com>
2022-08-30 00:21:36 +0300

    Add project files.

---
+++ Rehber/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace Rehber
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection baglanti = new SqlConnection(@"Data Source=DESKTOP-K0C08G8;Initial Catalog=Rehber;Integrated Security=True");
+
+        void Listele()
+        {
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter("Select * from KİSİLER", baglanti);
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            dataGridView1.Columns["FOTOGRAF"].Visible = false;
+        }
+
+        void Temizle()
+        {
+            TxtAd.Text = "";
+            TxtId.Text = "";
+            TxtSoyad.Text = "";
+            TxtMail.Text = "";
+            MskTelefon.Text = "";
+            TxtAd.Focus();
+            pictureBox1.ImageLocation = "";
+            TxtFoto.Text = " ";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Listele();
+        }
+
+        private void BtnEkle_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("Insert into KİSİLER (AD,SOYAD,TELEFON,MAIL,FOTOGRAF) values(@p1,@p2,@p3,@p4,@p5)", baglanti);
+            komut.Parameters.AddWithValue("@p1", TxtAd.Text);
+            komut.Parameters.AddWithValue("@p2", TxtSoyad.Text);
+            komut.Parameters.AddWithValue("@p3", MskTelefon.Text);
+            komut.Parameters.AddWithValue("@p4", TxtMail.Text);
+            komut.Parameters.AddWithValue("@p5", TxtFoto.Text);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Kişi Sisteme Kaydedildi.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Listele();
+            Temizle();
+        }
+
+        private void BtnTemizle_Click(object sender, EventArgs e)
+        {
+            Temizle();
+        }
+
+        private void BtnFotoEkle_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            pictureBox1.ImageLocation = openFileDialog1.FileName;
+            TxtFoto.Text = openFileDialog1.FileName;
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int secilen = dataGridView1.SelectedCells[0].RowIndex;
+            TxtId.Text = dataGridView1.Rows[secilen].Cells[0].Value.ToString();
+            TxtAd.Text = dataGridView1.Rows[secilen].Cells[1].Value.ToString();
+            TxtSoyad.Text = dataGridView1.Rows[secilen].Cells[2].Value.ToString();
+            MskTelefon.Text = dataGridView1.Rows[secilen].Cells[3].Value.ToString();
+            TxtMail.Text = dataGridView1.Rows[secilen].Cells[4].Value.ToString();
+            TxtFoto.Text = dataGridView1.Rows[secilen].Cells[5].Value.ToString();
+            pictureBox1.ImageLocation = TxtFoto.Text;
+        }
+
+        private void BtnSil_Click(object sender, EventArgs e)
+        {
+            var eminMisin = MessageBox.Show("Kişinin Bilgileri Silinsin Mi ?", "Uyarı", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning);
+            if (eminMisin == DialogResult.Yes)
+            {
+                baglanti.Open();
+                SqlCommand komut = new SqlCommand("Delete from KİSİLER where ID=" + TxtId.Text, baglanti);
+                komut.ExecuteNonQuery();
+                baglanti.Close();
+                MessageBox.Show("Kişi Rehberden Silindi", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                Listele();
+                Temizle();
+            }
+            else
+            {
+                MessageBox.Show("Silme İşlemi İptal Edildi", "Bilgi");
+            }
+        }
+
+        private void BtnGuncelle_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("Update KİSİLER set AD=@p1,SOYAD=@p2,TELEFON=@p3,MAIL=@p4,FOTOGRAF=@p5 where ID=@p6", baglanti);
+            komut.Parameters.AddWithValue("@p1", TxtAd.Text);
+            komut.Parameters.AddWithValue("@p2", TxtSoyad.Text);
+            komut.Parameters.AddWithValue("@p3", MskTelefon.Text);
+            komut.Parameters.AddWithValue("@p4", TxtMail.Text);
+            komut.Parameters.AddWithValue("@p5", TxtFoto.Text);
+            komut.Parameters.AddWithValue("@p6", TxtId.Text);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Kişi Bilgisi Güncellendi", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            Listele();
+            Temizle();
+        }
+
+       
+
+       
+    }
+}
